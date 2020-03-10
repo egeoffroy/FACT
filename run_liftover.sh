8f57095ece310fd1ada373fd4d4cec437415a8f7
Elyse Geoffroy <egeoffroy@luc.edu>
2020-03-10 16:02:44 -0500

    Update run_liftover.sh

--- run_liftover.sh
+++ run_liftover.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-Rscript Summary_Statistics/scripts/lifover_WojcikG.R --gwas /home/egeoffroy/Summary_Statistics/Summary_Stats/Wojcik/Body_mass_index/WojcikGL_31217584_GCST008025/SumStats/WojcikG_PMID_invn_rbmi_alls.gz -o /home/egeoffroy/Summary_Statistics/Summary_Stats/Wojcik/Body_mass_index/WojcikG_hg38.out
+GWAS=$1
+OUT=$2
+Rscript ~/liftover_GWAS_SS.R --gwas GWAS -o OUT
