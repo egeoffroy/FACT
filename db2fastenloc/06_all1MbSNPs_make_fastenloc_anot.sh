15e7460e5a3d8c87c1241a8f75e723d72990e99d
Elyse Geoffroy <egeoffroy@luc.edu>
2020-03-13 13:45:35 -0500

    Update 06_all1MbSNPs_make_fastenloc_anot.sh

--- db2fastenloc/06_all1MbSNPs_make_fastenloc_anot.sh
+++ db2fastenloc/06_all1MbSNPs_make_fastenloc_anot.sh
@@ -2,7 +2,9 @@
 
 #last step to make eQTL annotation file needed for fastenloc
 #-d is a directory that contatins all the dap-g output *fm.out files
-
-perl summarize_dap2enloc.pl -d AFA_all1Mb_dap_out/ -vcf AFA_cposid.vcf.gz | gzip - > AFA_all1Mb_fastenloc.eqtl.annotation.vcf.gz
+pop=$1
+perl summarize_dap2enloc.pl -d ${pop}_all1Mb_dap_out/ -vcf ${pop}_cposid.vcf.gz | gzip - > ${pop}_all1Mb_fastenloc.eqtl.annotation.vcf.gz
 
 #next, need to make sure SNP IDs in GWAS match this annotation file
+
+
