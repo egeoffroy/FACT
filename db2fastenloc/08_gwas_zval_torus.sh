1593d5bfccf531d1a33f6753dcf7d70a4b187390
Elyse Geoffroy <egeoffroy@luc.edu>
2020-03-13 13:59:41 -0500

    Update 08_gwas_zval_torus.sh

--- db2fastenloc/08_gwas_zval_torus.sh
+++ db2fastenloc/08_gwas_zval_torus.sh
@@ -2,11 +2,9 @@
 
 #run torus to get GWAS PIPs from zval file:
 #SNP Locus Z-score
+gwas_prefix=$1
+gzip ${gwas_prefix}.torus.zval.txt
+torus -d ${gwas_prefix}.torus.zval.txt.gz --load_zval -dump_pip ${gwas_prefix}.gwas.pip
+gzip ${gwas_prefix}.gwas.pip
 
-gzip Wojcik_height.torus.zval.txt
-torus -d Wojcik_height.torus.zval.txt.gz --load_zval -dump_pip Wojcik_height.gwas.pip
-gzip Wojcik_height.gwas.pip
 
-gzip Wojcik_WBC.torus.zval.txt
-torus -d Wojcik_WBC.torus.zval.txt.gz --load_zval -dump_pip Wojcik_WBC.gwas.pip
-gzip Wojcik_WBC.gwas.pip
