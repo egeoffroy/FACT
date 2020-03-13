b42500fb856cf645c9d5132f929660579875c856
Elyse Geoffroy <egeoffroy@luc.edu>
2020-03-13 13:32:03 -0500

    Update 03_all1MbSNPs_torus.sh

--- db2fastenloc/03_all1MbSNPs_torus.sh
+++ db2fastenloc/03_all1MbSNPs_torus.sh
@@ -2,26 +2,28 @@
 
 #run torus to get SNP priors based on distance to gene and
 #BFs from previous step
-
+geno_path=$1
+genemapfile=$2
+pop=$3
 #make snp.map file, want varID chr pos
 for i in {1..22}
 do
-    awk '{print $3 "\t" $1 "\t" $2}' /home/wheelerlab3/files_for_revisions_plosgen/en_v7/prepare_data/genotypes/AFA_${i}_annot.txt >> AFA.snp.map
+    awk '{print $3 "\t" $1 "\t" $2}' ${geno_path}${pop}_${i}_annot.txt >> ${pop}.snp.map
 done
 #rm lines with header & gzip
-grep -v varID AFA.snp.map | gzip > AFA.snp.map.gz
-rm AFA.snp.map
+grep -v varID ${pop}.snp.map | gzip > ${pop}.snp.map.gz
+rm ${pop}.snp.map
 
 #make gene.map file want gene_id chr start end
 for i in {1..22}
 do
-    awk '{print $2 "\t" $1 "\t" $4 "\t" $5}' /home/wheelerlab3/files_for_revisions_plosgen/en_v7/prepare_data/expression/gencode.v18.annotation.parsed.txt >> AFA.gene.map
+    awk '{print $2 "\t" $1 "\t" $4 "\t" $5}' ${genemapfile} >> ${pop}.gene.map
 done
 #rm lines with header & gzip
-grep -v gene_id AFA.gene.map | gzip > AFA.gene.map.gz
-rm AFA.gene.map
+grep -v gene_id ${pop}.gene.map | gzip > ${pop}.gene.map.gz
+rm ${pop}.gene.map
 
-torus -d AFA.all1Mb.bf.gz -smap AFA.snp.map.gz -gmap AFA.gene.map.gz --load_bf -dump_prior AFA_all1Mb_priors
+torus -d ${pop}.all1Mb.bf.gz -smap ${pop}.snp.map.gz -gmap ${pop}.gene.map.gz --load_bf -dump_prior ${pop}_all1Mb_priors
 
 #this gives different priors based on position, but assumes
 #position in third column is the TSS (it could be TES for some genes)
