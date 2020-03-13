509d5edeb4e3d3aed9d21f6acb6f69b34739bcaf
Elyse Geoffroy <egeoffroy@luc.edu>
2020-03-13 16:24:49 -0500

    Create run_04_all1MbSNPs_batch_dapg.sh

---
+++ db2fastenloc/run_scripts/run_04_all1MbSNPs_batch_dapg.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#PBS -N batch_dapg
+#PBS -S /bin/bash
+#PBS -l walltime=72:00:00
+#PBS -l nodes=1:ppn=32
+#PBS -l mem=64gb
+#PBS -o logs/${PBS_JOBNAME}.o.${PBS_JOBID}.log
+#PBS -e logs/${PBS_JOBNAME}.e.${PBS_JOBID}.err
+cd $PBS_O_WORKDIR
+pop=${pop}
+python3 04_all1MbSNPs_batch_dapg.py
+#run batch scan across 32 cores
+/usr/local/bin/openmp_wrapper -d ${pop}_all1Mb_batch_dap.cmd -t 32
+
+#Note this took ~7 hrs on 32 cpus
