414582da78aabccc622b451c3f3b6a05f3034a5a
Elyse Geoffroy <egeoffroy@luc.edu>
2020-03-13 14:01:27 -0500

    Update 09_all1MbSNPs_fastenloc.sh

--- db2fastenloc/09_all1MbSNPs_fastenloc.sh
+++ db2fastenloc/09_all1MbSNPs_fastenloc.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-
+gwas_prefix=$1
+pop=$2
 #run fastenloc with example sumstats
 #converted to pips by torus
 
-fastenloc -eqtl AFA_all1Mb_fastenloc.eqtl.annotation.vcf.gz -gwas Wojcik_height.gwas.pip.gz -prefix AFA_all1Mb_Wojcik_height
+fastenloc -eqtl ${pop}_all1Mb_fastenloc.eqtl.annotation.vcf.gz -gwas ${gwas_prefix}.gwas.pip.gz -prefix AFA_all1Mb_${gwas_prefix}
 
-fastenloc -eqtl AFA_all1Mb_fastenloc.eqtl.annotation.vcf.gz -gwas Wojcik_WBC.gwas.pip.gz -prefix AFA_all1Mb_Wojcik_WBC
+
