5ce0a513bfcb5cebeb7072841367bd0a9507f68b
Elyse Geoffroy <egeoffroy@luc.edu>
2020-03-13 13:25:41 -0500

    Update 04_all1MbSNPs_batch_dapg.py

--- db2fastenloc/04_all1MbSNPs_batch_dapg.py
+++ db2fastenloc/04_all1MbSNPs_batch_dapg.py
@@ -1,12 +1,22 @@
 #!/usr/bin/python3
 '''run dap-g using db SNPs for each gene'''
 import os
-datlist = os.listdir(path='AFA_all1Mb_sbams')
+import argparse
+def check_arg(args=None):
+    parser = argparse.ArgumentParser(description='Run dap-g using all SNPs for each gene')
+    parser.add_argument('-b', '--pop',
+                        help='group/pop id for group_id column of .dat file',
+                        required='True'
+                        )
+    return parser.parse_args(args)
+args = check_arg(sys.argv[1:])
+pop = args.pop
+datlist = os.listdir(path=pop+'_all1Mb_sbams')
 
-outfile = open("AFA_all1Mb_batch_dap.cmd","w")
-os.system("mkdir AFA_all1Mb_dap_out")
+outfile = open(pop+"_all1Mb_batch_dap.cmd","w")
+os.system("mkdir " +pop+"_all1Mb_dap_out")
 for f in datlist:
     ens = f[:-4]
-    outfile.write("/usr/local/bin/dap-g -d AFA_all1Mb_sbams/" + f + " -t 4 -p AFA_all1Mb_priors/" + ens + ".prior -o AFA_all1Mb_dap_out/" + ens + ".fm.out\n")
+    outfile.write("/usr/local/bin/dap-g -d "+pop+"_all1Mb_sbams/" + f + " -t 4 -p "+pop+"_all1Mb_priors/" + ens + ".prior -o "+pop+"_all1Mb_dap_out/" + ens $
 
 #consider adding -ld_control flag, default is -ld_control 0.25
