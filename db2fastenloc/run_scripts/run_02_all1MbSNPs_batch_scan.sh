7ec2430da073566a821d59ff4d20560ce916df61
Elyse Geoffroy <egeoffroy@luc.edu>
2020-03-15 13:48:43 -0500

    Update run_02_all1MbSNPs_batch_scan.sh

--- db2fastenloc/run_scripts/run_02_all1MbSNPs_batch_scan.sh
+++ db2fastenloc/run_scripts/run_02_all1MbSNPs_batch_scan.sh
@@ -6,7 +6,7 @@
 #PBS -l mem=32gb
 #PBS -o logs/${PBS_JOBNAME}.o.${PBS_JOBID}.log
 #PBS -e logs/${PBS_JOBNAME}.e.${PBS_JOBID}.err
-cd $PBS_O_WORKDIR
+#cd $PBS_O_WORKDIR
 pop=$1
 mkdir ${pop}_all1Mb_scan_out
 python3 02_all1MbSNPs_batch_scan.py
