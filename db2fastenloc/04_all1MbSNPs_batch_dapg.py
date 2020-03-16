fcd2a2cb44b36682f711797e0d6f1b0b309d304f
Elyse Geoffroy <egeoffroy@luc.edu>
2020-03-16 10:04:13 -0500

    Update 04_all1MbSNPs_batch_dapg.py

--- db2fastenloc/04_all1MbSNPs_batch_dapg.py
+++ db2fastenloc/04_all1MbSNPs_batch_dapg.py
@@ -17,6 +17,6 @@ outfile = open(pop+"_all1Mb_batch_dap.cmd","w")
 os.system("mkdir " +pop+"_all1Mb_dap_out")
 for f in datlist:
     ens = f[:-4]
-    outfile.write("/usr/local/bin/dap-g -d "+pop+"_all1Mb_sbams/" + f + " -t 4 -p "+pop+"_all1Mb_priors/" + ens + ".prior -o "+pop+"_all1Mb_dap_out/" + ens $
+    outfile.write("/usr/local/bin/dap-g -d "+pop+"_all1Mb_sbams/" + f + " -t 4 -p "+pop+"_all1Mb_priors/" + ens + ".prior -o "+pop+"_all1Mb_dap_out/" + ens + ".fm.out\n")
 
 #consider adding -ld_control flag, default is -ld_control 0.25
