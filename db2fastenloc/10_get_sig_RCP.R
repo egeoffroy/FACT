9dbec8490998a406911191cdbb49c6325041e926
Elyse Geoffroy <egeoffroy@luc.edu>
2020-03-23 08:33:50 -0500

    Create 10_get_sig_RCP.R

---
+++ db2fastenloc/10_get_sig_RCP.R
@@ -0,0 +1,10 @@
+library(dplyr)
+library(data.table)
+
+args = commandArgs(trailingOnly=TRUE)
+prefix = args[1]
+pop = args[2]
+data <- fread(paste(prefix, ".enloc.sig.out", sep = ''), header =F)
+print(head(data))
+data <- data %>% filter(V6 >= 0.5)
+write.table(data, paste(prefix, "significant_rcp.txt", sep=''), quote=F, col.names=F, row.names=F)
