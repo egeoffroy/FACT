8e5d54ed1aff6c4baad477ff2fae762cff141cd2
Elyse Geoffroy <egeoffroy@luc.edu>
2020-03-30 10:38:55 -0500

    Update run_02_all1MbSNPs_batch_scan.sh
    needed to add pop flag to 02b call as it wasn't running properly

--- db2fastenloc/run_scripts/run_02_all1MbSNPs_batch_scan.sh
+++ db2fastenloc/run_scripts/run_02_all1MbSNPs_batch_scan.sh
@@ -13,5 +13,5 @@ python3 02_all1MbSNPs_batch_scan.py --pop ${pop}
 #run batch scan across 32 cores
 /usr/local/bin/openmp_wrapper -d ${pop}_all1Mb_batch_scan.cmd -t 32
 #combine output files
-python3 02b_concat_scan_out_bf_files.py
+python3 02b_concat_scan_out_bf_files.py --pop ${pop}
 #Note this took ~7.5 hrs on 32 cpus
