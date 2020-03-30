e69740fe5ee3ab9e0710b984b0d8bfd85eea06d2
Elyse Geoffroy <egeoffroy@luc.edu>
2020-03-30 11:29:31 -0500

    Update main.py
    added 10th step also fixed some of the issues so that we can run both sides of the fastenloc --> doing more gwas analysis without having to re-run all of the eqtl steps

--- db2fastenloc/main.py
+++ db2fastenloc/main.py
@@ -1,31 +1,22 @@
 import os
 import sys
 import argparse
-#Example Files --will be added to CompBio server over weekend
-#pop = YRI
-#outdir for 01 = pop + 'sbams/'
+
 def check_arg(args=None):
     parser = argparse.ArgumentParser(description='Matrix EQTL to .dat format from .db SNP list')
     parser.add_argument('-g', '--geno',
-                        help='input genotype folder',
-                        required='True'
+                        help='input genotype folder'
                         )
     parser.add_argument('-p', '--pheno',
-                        help='input phenotype file',
-                        required='True'
+                        help='input phenotype file'
                         )
     parser.add_argument('-m', '--genemap',
-                        help='gene position map',
-                        required='True'
+                        help='gene position map'
                         )
     parser.add_argument('-b', '--pop',
                         help='group/pop id for group_id column of .dat file',
                         required='True'
                         )
-    #parser.add_argument('-n', '--gwas_n', default=1,
-    #                    help='Number of GWAS Summary Statistics Files With Same Format',
-    #                    required='True'
-    #                    )
     parser.add_argument('-gwas', '--gwas_SS',
                         help='GWAS Summary Statistics File',
                         required='True'
@@ -42,34 +33,32 @@ def check_arg(args=None):
 
 #retrieve command line arguments
 args = check_arg(sys.argv[1:])
-geno_folder = args.geno
-phenofile = args.pheno
-genemapfile = args.genemap
 pop = args.pop
-#gwas_n = args.gwas_n #This may not be needed 
 gwasSS = args.gwas_SS
-#gwas_n = len(gwasSS)
 LD = args.LD
 gwas_prefix = args.gwas_out_prefixes
 
-if geno :
+if args.geno :
+    geno_folder = args.geno
+    phenofile = args.pheno
+    genemapfile = args.genemap
     os.system('mkdir ' + pop + '_all1Mb_sbams')
     os.system('python3 run_scripts/make_run_scripts_01.py --geno '+geno_folder+' --pheno '+phenofile+' --genemap '+genemapfile+' --pop '+pop+' --outdir ' + pop + '_all1Mb_sbams')
     #work on timing between steps to prevent the program from going over steps before files are ready
     os.system('bash nohup_01.txt')
-    #os.system('python3 02_all1MbSNPs_batch_scan.py --pop ' + pop)
     os.system('bash run_scripts/run_02_all1MbSNPs_batch_scan.sh ' + pop)
     os.system('python3 02b_concat_scan_out_bf_files.py --pop ' + pop)
     os.system('bash 03_all1MbSNPs_torus.sh ' + geno_folder + ' ' + genemapfile + ' ' + pop)
     os.system('bash run_scripts/run_04_all1MbSNPs_batch_dapg.sh ' + pop)
     os.system('bash run_scripts/run_05_make_vcf.py  '+ geno_folder + ' ' + pop)
-    os.system('bash 06_all1MbSNPs_make_fastenloc_anot.sh ' + pop) #Run script 06
+    os.system('bash 06_all1MbSNPs_make_fastenloc_anot.sh ' + pop) 
 
 if gwasSS: 
-    for i in gwasSS:
-        os.system('Rscript 07a_sumstats_names.R ' + gwasSS[i])
-        os.system('python3 07_prep_sumstats_1000G_LDblocks.py --ldblocks ' + LD + '--s ' + gwasSS[i] + ' --pop ' + pop + ' --annot ' + pop + '_all1Mb_fastenloc.eqtl.annotation.vcf.gz' + ' --outprefix ' + gwas_prefix[i])
-        os.system('bash 08_gwas_zval_torus.sh ' + gwas_prefix[i] + ' ' + pop)
-        os.system('bash 09_all1MbSNPs_fastenloc.sh ' + gwas_prefix[i] + ' ' + pop)
+        os.system('Rscript 07a_sumstats_names.R ' + gwasSS)
+        os.system('python3 07_prep_sumstats_1000G_LDblocks.py --ldblocks ' + LD + '--s ' + gwasSS + ' --pop ' + pop + ' --annot ' + pop + '_all1Mb_fastenloc.eqtl.annotation.vcf.gz' + ' --outprefix ' + gwas_prefix)
+        os.system('bash 08_gwas_zval_torus.sh ' + gwas_prefix + ' ' + pop)
+        os.system('bash 09_all1MbSNPs_fastenloc.sh ' + gwas_prefix + ' ' + pop)
+        os.system('Rscript 10_get_sig_RCP.R ' + gwas_prefix + ' ' + pop)
+        
 
 
