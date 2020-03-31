b7e480b864611a41447319e7b0991bd4760d3ebf
Elyse Geoffroy <egeoffroy@luc.edu>
2020-03-31 15:46:50 -0500

    Update coloc_pipeline_main.py

--- coloc/coloc_pipeline_main.py
+++ coloc/coloc_pipeline_main.py
@@ -2,37 +2,8 @@ import os
 import sys
 import argparse
 import subprocess
-
-def check_arg(args=None):
-    parser = argparse.ArgumentParser(description='Run coloc for various GWAS SS')
-    parser.add_argument('-pid', '--pheno_id',
-                        help='input phenotype id',
-                        required='True'
-                        )
-    parser.add_argument('-b', '--pop',
-                        help='population',
-                        required='True'
-                        )
-    parser.add_argument('-gwas', '--gwas_SS',
-                        help='GWAS Summary Statistics File',
-                        required='True'
-                        )
-    parser.add_argument('-meqtl', '--meqtl',
-                        help='meQTL file'
-                        )
-    parser.add_argument('-frq', '--frq',
-                        help='.frq file',
-                        required='True'
-                        )
-    return parser.parse_args(args)
-
-#retrieve command line arguments
-args = check_arg(sys.argv[1:])
-phenoid = argimport os
-import sys
-import argparse
-import subprocess
-
+import os.path
+from os import path
 def check_arg(args=None):
     parser = argparse.ArgumentParser(description='Run coloc for various GWAS SS')
     parser.add_argument('-pid', '--pheno_id',
@@ -71,16 +42,18 @@ populations = {
   "YRI": 107
 }
 
-os.system('mkdir coloc')
-command = 'Rscript SNP_list.R ' + gwasSS + ' ' + phenoid
-result = subprocess.getoutput(command)
-
-if args.meqtl:
-    os.system('Rscript make_coloc_files.R ' + gwasSS + ' ' + frqfile + ' ' + phenoid + ' ' + result + ' ' + pop + ' ' + str(populations.get(pop)) + ' ' + args.meqtl) #currently only for MESA models
+if path.isdir('coloc'):
+        print('Directory coloc exists')
 else:
-    os.system('Rscript make_coloc_files.R ' + gwasSS + ' ' + frqfile + ' ' + phenoid + ' ' + result + ' ' + pop + ' ' + str(populations.get(pop))) #currently only for MESA models
+        os.system('mkdir coloc')
+
+command = 'Rscript SNP_list.R ' + gwasSS + ' ' + phenoid
+#result = subprocess.getoutput(command)
+#print(result)
+if args.meqtl:
+    os.system('Rscript make_coloc_files.R ' + gwasSS + ' ' + frqfile + ' ' + phenoid + ' ' + pop + ' ' + str(populations.get(pop)) + ' ' + args.meqtl) #currently only for MESA models
+else:
+    os.system('Rscript make_coloc_files.R ' + gwasSS + ' ' + frqfile + ' ' + phenoid + ' ' + pop + ' ' + str(populations.get(pop))) #currently only for MESA models
 
 
-
-
-os.system('bash run_coloc.sh ' + pop + ' ' + phenoid + ' ' + str(populations.get(pop)))
+os.system('bash /home/elyse/coloc/run_pipeline_1.sh ' + pop + ' ' + phenoid + ' ' + str(populations.get(pop)))
