10695fa05a70f76bc24ffbbdbae4b44b7edd6162
Elyse Geoffroy <egeoffroy@luc.edu>
2020-03-31 11:21:21 -0500

    Create coloc_pipeline_main.py

---
+++ coloc/coloc_pipeline_main.py
@@ -0,0 +1,86 @@
+import os
+import sys
+import argparse
+import subprocess
+
+def check_arg(args=None):
+    parser = argparse.ArgumentParser(description='Run coloc for various GWAS SS')
+    parser.add_argument('-pid', '--pheno_id',
+                        help='input phenotype id',
+                        required='True'
+                        )
+    parser.add_argument('-b', '--pop',
+                        help='population',
+                        required='True'
+                        )
+    parser.add_argument('-gwas', '--gwas_SS',
+                        help='GWAS Summary Statistics File',
+                        required='True'
+                        )
+    parser.add_argument('-meqtl', '--meqtl',
+                        help='meQTL file'
+                        )
+    parser.add_argument('-frq', '--frq',
+                        help='.frq file',
+                        required='True'
+                        )
+    return parser.parse_args(args)
+
+#retrieve command line arguments
+args = check_arg(sys.argv[1:])
+phenoid = argimport os
+import sys
+import argparse
+import subprocess
+
+def check_arg(args=None):
+    parser = argparse.ArgumentParser(description='Run coloc for various GWAS SS')
+    parser.add_argument('-pid', '--pheno_id',
+                        help='input phenotype id',
+                        required='True'
+                        )
+    parser.add_argument('-b', '--pop',
+                        help='population',
+                        required='True'
+                        )
+    parser.add_argument('-gwas', '--gwas_SS',
+                        help='GWAS Summary Statistics File',
+                        required='True'
+                        )
+    parser.add_argument('-meqtl', '--meqtl',
+                        help='meQTL file'
+                        )
+    parser.add_argument('-frq', '--frq',
+                        help='.frq file',
+                        required='True'
+                        )
+    return parser.parse_args(args)
+
+#retrieve command line arguments
+args = check_arg(sys.argv[1:])
+phenoid = args.pheno_id
+pop = args.pop
+frqfile = args.frq
+gwasSS = args.gwas_SS
+populations = {
+  "AFA": 233,
+  "AFHI": 585,
+  "CAU": 578,
+  "HIS": 352,
+  "ALL": 1163,
+  "YRI": 107
+}
+
+os.system('mkdir coloc')
+command = 'Rscript SNP_list.R ' + gwasSS + ' ' + phenoid
+result = subprocess.getoutput(command)
+
+if args.meqtl:
+    os.system('Rscript make_coloc_files.R ' + gwasSS + ' ' + frqfile + ' ' + phenoid + ' ' + result + ' ' + pop + ' ' + str(populations.get(pop)) + ' ' + args.meqtl) #currently only for MESA models
+else:
+    os.system('Rscript make_coloc_files.R ' + gwasSS + ' ' + frqfile + ' ' + phenoid + ' ' + result + ' ' + pop + ' ' + str(populations.get(pop))) #currently only for MESA models
+
+
+
+
+os.system('bash run_coloc.sh ' + pop + ' ' + phenoid + ' ' + str(populations.get(pop)))
