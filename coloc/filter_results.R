0aeef7127a96d5feff397b793526254f16352e25
Elyse Geoffroy <egeoffroy@luc.edu>
2020-04-01 09:24:32 -0500

    Update filter_results.R

--- coloc/filter_results.R
+++ coloc/filter_results.R
@@ -9,7 +9,7 @@ sig_genes_file <- args[3]
 coloc_file <- "output_" %&% pheno %&% '_' %&% pop %&% '_coloc.txt.gz'
 coloc <- fread(coloc_file, header = T, stringsAsFactors = F)
 sig_genes <- fread(sig_genes_file, header = T, stringsAsFactors = F)
-sig_genes <- sig_genes$PROBE_ID
+sig_genes <- sig_genes$GENE
 coloc <- coloc %>% filter(gene_id %in% sig_genes)
 print(head(coloc))
 output <- 'sig_genes_coloc_' %&% pop %&% '_' %&% pheno %&% '.csv'
