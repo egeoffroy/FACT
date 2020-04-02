6ab7eb04cd3e62ef8572c17774ce086246d72245
Elyse Geoffroy <egeoffroy@luc.edu>
2020-04-02 08:54:35 -0500

    Create finemap.py

---
+++ coloc/finemap.py
@@ -0,0 +1,11 @@
+# This script is the new main wrapper once the COLOC features are added to the overall pipeline
+
+import os
+import sys
+import subprocess
+import argparse 
+
+if args.coloc:
+
+
+if args.fastenloc: 
