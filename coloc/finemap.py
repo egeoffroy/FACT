7544978cdfacd3920d9e6ba2ee13a6e10aaa90dd
Elyse Geoffroy <egeoffroy@luc.edu>
2020-04-02 09:00:18 -0500

    Update finemap.py

--- coloc/finemap.py
+++ coloc/finemap.py
@@ -5,7 +5,48 @@ import sys
 import subprocess
 import argparse 
 
+def check_arg(args=None):
+    parser = argparse.ArgumentParser(description='Run fastenloc or coloc for various GWAS SS')
+    parser.add_argument('-coloc', '--coloc',
+                        help='input test type'
+                        )
+    parser.add_argument('-fastenloc', '--fastenloc',
+                        help='input test type'
+                        )
+    parser.add_argument('-pid', '--pheno_id',
+                        help='input phenotype id',
+                        required='True'
+                        )
+    parser.add_argument('-b', '--pop',
+                        help='population',
+                        required='True'
+                        )
+    parser.add_argument('-gwas', '--gwas_SS',
+                        help='GWAS Summary Statistics File',
+                        required='True'
+                        )
+    parser.add_argument('-meqtl', '--meqtl',
+                        help='meQTL file'
+                        )
+    parser.add_argument('-frq', '--frq',
+                        help='.frq file',
+                        required='True'
+                        )
+    parser.add_argument('-filter_by', '--filter_by',
+                        help='filter by this file of significant genes from S-PrediXcan/PrediXcan output. Requires gene column'
+                        )
+    return parser.parse_args(args)
+
+#retrieve command line arguments
+args = check_arg(sys.argv[1:])
+
 if args.coloc:
+    if args.meqtl: 
+            os.system('python3 ./coloc/coloc_pipeline_main.py --pop ' + args.pop + ' --gwas_SS ' + args.gwas_SS + ' --frq ' + args.frq + ' --pheno_id ' + args.pheno_id + ' --meqtl ' + args.meqtl)
+            if args.filter_by:
+                os.system('python3 ./coloc/coloc_pipeline_main.py --pop ' + args.pop + ' --gwas_SS ' + args.gwas_SS + ' --frq ' + args.frq + ' --pheno_id ' + args.pheno_id + ' --filter_by ' + args.filter_by + ' --meqtl ' + args.meqtl)
+    if args.filter_by:
+            os.system('python3 ./coloc/coloc_pipeline_main.py --pop ' + args.pop + ' --gwas_SS ' + args.gwas_SS + ' --frq ' + args.frq + ' --pheno_id ' + args.pheno_id + ' --filter_by ' + args.filter_by)
+    os.system('python3 ./coloc/coloc_pipeline_main.py --pop ' + args.pop + ' --gwas_SS ' + args.gwas_SS + ' --frq ' + args.frq + ' --pheno_id ' + args.pheno_id)
 
-
-if args.fastenloc: 
+#if args.fastenloc: 
