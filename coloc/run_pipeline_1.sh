c820544f917a8ee9a831c7647a0ad6a151524009
Elyse Geoffroy <egeoffroy@luc.edu>
2020-04-07 16:59:01 -0500

    Create run_pipeline_1.sh

---
+++ coloc/run_pipeline_1.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+pop=$1
+pheno=$2
+pop_size=$3
+time python3 summary-gwas-imputation/src/run_coloc.py \
+-gwas_mode bse \
+-gwas coloc/GWAS_${pop}_${pheno}.txt.gz \
+-eqtl_mode bse \
+-eqtl coloc/eQTL_${pop}_${pheno}.txt.gz \
+-gwas_sample_size FROM_GWAS \
+-eqtl_sample_size ${pop_size} \
+-parsimony 8 \
+-output coloc/output_${pop}_${pheno}_coloc.txt.gz  > /dev/null
