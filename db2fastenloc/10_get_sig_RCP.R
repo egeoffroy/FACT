054fba2a5f9946aab7adb4dabc461f60924013fa
Elyse Geoffroy <egeoffroy@luc.edu>
2020-04-08 11:25:05 -0500

    Update 10_get_sig_RCP.R

--- db2fastenloc/10_get_sig_RCP.R
+++ db2fastenloc/10_get_sig_RCP.R
@@ -7,5 +7,6 @@ prefix = args[1]
 pop = args[2]
 data <- fread(paste(prefix, ".enloc.sig.out", sep = ''), header =F)
 print(head(data))
-data <- data %>% filter(V6 >= 0.5)
-write.table(data, paste(prefix, "significant_rcp.txt", sep=''), quote=F, col.names=F, row.names=F)
+data <- data[order(V6),]
+#data <- data %>% filter(V6 >= 0.5)
+write.table(data[1:10,], paste(prefix, "significant_rcp.txt", sep=''), quote=F, col.names=F, row.names=F)
