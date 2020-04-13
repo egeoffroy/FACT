b7a0dc0cc9a722ba1f97b02e67fe7248634fa708
Elyse Geoffroy <egeoffroy@luc.edu>
2020-04-13 11:31:11 -0500

    Update finemap.py

--- finemap.py
+++ finemap.py
@@ -63,11 +63,10 @@ if args.coloc:
             os.system('python3 coloc_pipeline_main.py --pop ' + args.pop + ' --gwas_SS ' + args.gwas_SS + ' --frq ' + args.frq + ' --pheno_id ' + args.pheno_id + ' --filter_by ' + args.filter_by)
     os.system('python3 coloc_pipeline_main.py --pop ' + args.pop + ' --gwas_SS ' + args.gwas_SS + ' --frq ' + args.frq + ' --pheno_id ' + args.pheno_id)
 
-if args.fastenloc: # This is currently broken because of the chromosome thing... will have to fix
+if args.fastenloc: # Updated: works
     os.chdir('./db2fastenloc')
     if args.chr:
          start = args.chr[0]
          stop = args.chr[1]
     make_cmd = 'python3 main.py --geno {} --meqtl {} --genemap {} --pop {} --gwas_SS {} --LD {} --start {} --stop {} --gwas_out_prefixes {}'.format(args.geno, args.meqtl, args.genemap, args.pop, args.gwas_SS, args.LD, start, stop, args.pheno_id)
     os.system(make_cmd)
-    #os.system('python3 main.py --geno ' + args.geno + ' --pheno ' + args.meqtl + ' --genemap ' + args.genemap + ' --pop ' + args.pop + ' --gwas_SS ' + args.gwas_SS +' --LD ' + args.LD +' --start ' + start + ' --stop ' + stop + ' --gwas_out_prefixes '+ args.pheno_id)
