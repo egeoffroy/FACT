6091096efd90d470c66a940e6a719b4e5ac81e37
Elyse Geoffroy <egeoffroy@luc.edu>
2020-04-14 10:00:19 -0500

    Update id_sig_hits.py

--- db2fastenloc/id_sig_hits.py
+++ db2fastenloc/id_sig_hits.py
@@ -7,11 +7,15 @@ def check_arg(args=None):
                         help='group/pop id for group_id column of .dat file',
                         required='True'
                         )
+    parser.add_argument('-prefix', '--prefix',
+                        help='group/pop id for group_id column of .dat file',
+                        required='True'
+                        )
 args = check_arg(sys.argv[1:])
 pop = args.pop
 import csv
 
-input_file = '{}_gwasprefix.enloc.sig.out'.format(pop)
+input_file = '{}_{}.enloc.sig.out'.format(pop, args.prefix)
 with open(input_file, 'r') as results:
     head =[next(results) for x in range(10)]
 for line in head:
