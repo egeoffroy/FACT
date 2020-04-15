3da937ff586e911aabcb8a28243265f4dddf5e80
Kathleen Delany <54127089+katiemdelany@users.noreply.github.com>
2020-04-15 14:51:33 -0500

    Update id_sig_hits.py

--- db2fastenloc/id_sig_hits.py
+++ db2fastenloc/id_sig_hits.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import os
 import sys
+import shlex
 #create files that identify top ten RCP hits
 def check_arg(args=None):
     parser = argparse.ArgumentParser(description='Matrix EQTL to .dat format from .db SNP list')
@@ -14,14 +15,14 @@ def check_arg(args=None):
                         )
 args = check_arg(sys.argv[1:])
 pop = args.pop
+
 import csv
 
 input_file = '{}_{}.enloc.sig.out'.format(pop, args.prefix)
 with open(input_file, 'r') as results:
-    head =[next(results) for x in range(10)]
-for line in head:
-    data = csv.reader(head, delimiter='\t')
+    data =csv.reader(results, delimiter='\t')
+    sortData = sorted(data, key=lambda x: int(x[5]), reverse=True)
 with open('significant_RCP.txt','w') as outfile:
-    for i in data:
-        outfile.write(i[5] + '\n')
-outfile.close()
+    for i in range(10):
+        outfile.write(str('\t'.join([str(x) for x in sortData[i]]))+'\n')
+outfile.close() 
