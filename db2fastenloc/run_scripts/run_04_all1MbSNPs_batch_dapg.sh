b6fd55cbf5203ba6d36fddd5b74c55cdf6007e36
Elyse Geoffroy <egeoffroy@luc.edu>
2020-04-20 13:38:53 -0500

    Update run_04_all1MbSNPs_batch_dapg.sh

--- db2fastenloc/run_scripts/run_04_all1MbSNPs_batch_dapg.sh
+++ db2fastenloc/run_scripts/run_04_all1MbSNPs_batch_dapg.sh
@@ -10,6 +10,6 @@
 pop=$1
 python3 04_all1MbSNPs_batch_dapg.py --pop ${pop}
 #run batch scan across 32 cores
-/usr/local/bin/openmp_wrapper -d ${pop}_all1Mb_batch_dap.cmd -t 4
+/usr/local/bin/openmp_wrapper -d ${pop}_all1Mb_batch_dap.cmd -t 16
 
 #Note this took ~7 hrs on 32 cpus
