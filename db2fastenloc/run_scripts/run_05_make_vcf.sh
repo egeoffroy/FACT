aa9bee8cb0ae1772abc6462d292d448f9140bfef
Elyse Geoffroy <egeoffroy@luc.edu>
2020-04-22 13:09:14 -0500

    Update run_05_make_vcf.sh

--- db2fastenloc/run_scripts/run_05_make_vcf.sh
+++ db2fastenloc/run_scripts/run_05_make_vcf.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
-#PBS -N make_vcf
-#PBS -S /bin/bash
-#PBS -l walltime=24:00:00
-#PBS -l nodes=1:ppn=4
-#PBS -l mem=16gb
-#PBS -o logs/${PBS_JOBNAME}.o.${PBS_JOBID}.log
-#PBS -e logs/${PBS_JOBNAME}.e.${PBS_JOBID}.err
-#cd $PBS_O_WORKDIR
 geno=$1
 pop=$2
 
