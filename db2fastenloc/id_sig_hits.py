6519da097f2affc1fc81bedca676e6f7ac80a08d
Kathleen Delany <54127089+katiemdelany@users.noreply.github.com>
2020-04-22 14:08:00 -0500

    Update id_sig_hits.py

--- db2fastenloc/id_sig_hits.py
+++ db2fastenloc/id_sig_hits.py
@@ -3,7 +3,11 @@ import os
 import sys
 import shlex
 import argparse
-#create files that identify top ten RCP hits
+'''
+Input: fastenloc sig.out file 
+Output: Text file with top ten results, sorted by Regional Colocalization Probability
+
+'''
 def check_arg(args=None):
     parser = argparse.ArgumentParser(description='Matrix EQTL to .dat format from .db SNP list')
     parser.add_argument('-b', '--pop',
@@ -20,8 +24,10 @@ args = check_arg(sys.argv[1:])
 pop = args.pop
 
 import csv
-
+#input fastenloc sig.out file
 input_file = '{}_all1Mb_{}.enloc.sig.out'.format(pop, args.prefix)
+#read in results as tab-delimited csv
+#add each line of results to a list
 with open(input_file, 'r') as results:
     data2 =csv.reader(results, delimiter='\t')
     data = []
@@ -29,7 +35,8 @@ with open(input_file, 'r') as results:
         for j in i:
             j = j.split()
         data.append(j)
-    sortData = sorted(data, key=lambda x: float(x[5]), reverse=True)
+    sortData = sorted(data, key=lambda x: float(x[5]), reverse=True) 
+#output sorted results list to text file
 with open('significant_RCP.txt','w') as outfile:
     for i in range(10):
         outfile.write(str('\t'.join([str(x) for x in sortData[i]]))+'\n')
