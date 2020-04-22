a3e46532d09907278c43c90cf2e33884ebbd28d9
Elyse Geoffroy <egeoffroy@luc.edu>
2020-04-22 13:03:16 -0500

    Update coloc_pipeline_main.py

--- coloc/coloc_pipeline_main.py
+++ coloc/coloc_pipeline_main.py
@@ -4,7 +4,7 @@ import argparse
 import subprocess
 import os.path
 from os import path
-def check_arg(args=None):
+def check_arg(args=None): #get arguments for COLOC
     parser = argparse.ArgumentParser(description='Run coloc for various GWAS SS')
     parser.add_argument('-pid', '--pheno_id',
                         help='input phenotype id',
@@ -35,33 +35,27 @@ def check_arg(args=None):
 
 #retrieve command line arguments
 args = check_arg(sys.argv[1:])
-phenoid = args.pheno_id
+phenoid = args.pheno_id #assign arguments to variables
 pop = args.pop
 frqfile = args.frq
 gwasSS = args.gwas_SS
 pop_size = args.pop_size
-#populations = {
-#  "AFA": 233,
-#  "AFHI": 585,
-#  "CAU": 578,
-#  "HIS": 352,
-#  "ALL": 1163,
-#  "YRI": 107
-#}
+
 
 if path.isdir('coloc'):
-        print('Directory coloc exists')
+        print('Directory coloc exists') 
 else:
-        os.system('mkdir coloc')
+        os.system('mkdir coloc') #make output directory if not already existing
 
-command = 'Rscript SNP_list.R ' + gwasSS + ' ' + phenoid
+command = 'Rscript SNP_list.R ' + gwasSS + ' ' + phenoid #get SNP list
 result = subprocess.getoutput(command)
 
-if args.meqtl:
+#make coloc input files --> format eQTL and GWAS data
+if args.meqtl: #if user inputs meqtl file
     os.system('Rscript make_coloc_files.R ' + gwasSS + ' ' + frqfile + ' ' + phenoid + ' ' + pop + ' ' + pop_size + ' ' + args.meqtl) 
 else:
     os.system('Rscript make_coloc_files.R ' + gwasSS + ' ' + frqfile + ' ' + phenoid + ' ' + pop + ' ' + pop_size) 
-os.system('bash run_pipeline_1.sh ' + pop + ' ' + phenoid + ' ' + str(populations.get(pop)))
+os.system('bash run_pipeline_1.sh ' + pop + ' ' + phenoid + ' ' + str(populations.get(pop))) #run COLOC
 
-if args.filter_by:
+if args.filter_by: #filter results in user includes filtering PrediXcan significant genes file
     os.system('Rscript filter_results.R ' + phenoid + ' ' + pop + ' ' + args.filter_by)
